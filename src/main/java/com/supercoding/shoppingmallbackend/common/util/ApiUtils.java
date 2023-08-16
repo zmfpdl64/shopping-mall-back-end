74f85889b735f8120f5d4577fcd5430a3d98947d
koeyismyusername <112312898+koeyismyusername@users.noreply.github.com>
2023-08-16 16:11:53 +0900

    # Refactor: ApiUtils 확장

--- src/main/java/com/supercoding/shoppingmallbackend/common/util/ApiUtils.java
+++ src/main/java/com/supercoding/shoppingmallbackend/common/util/ApiUtils.java
@@ -7,11 +7,19 @@ public class ApiUtils {
 
     private ApiUtils(){}
 
-    public static CommonResponse<Object> success(String message, Object data) {
-        return new CommonResponse<>(true ,200, message, data);
+//    public static CommonResponse<Object> success(String message, Object data) {
+//        return new CommonResponse<>(true ,200, message, data);
+//    }
+
+    public static <T> CommonResponse<T> success(String message, T data) {
+        return new CommonResponse<>(true, 200, message, data);
     }
 
-    public static CommonResponse<Object> fail(int code, String message) {
-        return new CommonResponse<>(false, code, message ,null);
+//    public static CommonResponse<Object> fail(int code, String message) {
+//        return new CommonResponse<>(false, code, message ,null);
+//    }
+
+    public static <T> CommonResponse<T> fail(Integer status, String message) {
+        return new CommonResponse<>(false, status, message, null);
     }
 }
