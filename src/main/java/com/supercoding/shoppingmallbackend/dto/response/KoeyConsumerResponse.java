e0e80b837d526e8037566e3b6655e8db41f77687
koeyismyusername <112312898+koeyismyusername@users.noreply.github.com>
2023-08-16 16:15:54 +0900

    # Feat: 임시로 사용할 dto 추가

---
+++ src/main/java/com/supercoding/shoppingmallbackend/dto/response/KoeyConsumerResponse.java
@@ -0,0 +1,13 @@
+package com.supercoding.shoppingmallbackend.dto.response;
+
+import lombok.*;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+public class KoeyConsumerResponse {
+    private Long id;
+    private Long profileId;
+}
