9e4920cf03952b6f7e2ecc4927b2afa34cf63f2d
koeyismyusername <112312898+koeyismyusername@users.noreply.github.com>
2023-08-17 22:28:18 +0900

    Fix: Profile 반환하는 쿼리 제거 Refactor: consumerId로 consumer 찾아오는 쿼리 작성

--- src/main/java/com/supercoding/shoppingmallbackend/repository/ConsumerRepository.java
+++ src/main/java/com/supercoding/shoppingmallbackend/repository/ConsumerRepository.java
@@ -1,6 +1,7 @@
 package com.supercoding.shoppingmallbackend.repository;
 
 import com.supercoding.shoppingmallbackend.entity.Consumer;
+import com.supercoding.shoppingmallbackend.entity.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -10,7 +11,6 @@ import java.util.Optional;
 @Repository
 public interface ConsumerRepository extends JpaRepository<Consumer, Long> {
 
-    Optional<Consumer> findByIdAndIsDeletedIsFalse(Long consumerId);
-    @Query("select p from Consumer c join fetch c.profile p where c.isDeleted=false and p.isDeleted=false and c.id=:consumerId limit 1")
-    Optional<Consumer> findProfileByConsumerId(Long consumerId);
+    @Query("select c from Consumer c join fetch c.profile p where c.id=:consumerId and c.isDeleted=false and p.isDeleted=false")
+    Optional<Consumer> findConsumerById(Long consumerId);
 }
