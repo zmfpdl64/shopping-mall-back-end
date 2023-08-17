4b167fe98b87affca66025c2bfa27df8ad49dc17
koeyismyusername <112312898+koeyismyusername@users.noreply.github.com>
2023-08-18 08:14:19 +0900

    Feat: 판매내역 조회 쿼리 추가 Refactor: 쿼리문 가독성 향상을 위해 줄바꿈

--- src/main/java/com/supercoding/shoppingmallbackend/repository/PaymentRepository.java
+++ src/main/java/com/supercoding/shoppingmallbackend/repository/PaymentRepository.java
@@ -11,9 +11,33 @@ import java.util.List;
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
     boolean existsByOrderNumber(String orderNumber);
 
-    @Query("select p from Payment p join fetch p.consumer c join fetch p.product pd where p.orderNumber=:orderNumber and p.isDeleted=false order by p.paidAt desc")
+    @Query("select p from Payment p " +
+            "join fetch p.consumer c " +
+            "join fetch c.profile pf " +
+            "join fetch p.product pd " +
+            "join fetch pd.seller s " +
+            "join fetch pd.genre g " +
+            "where p.orderNumber=:orderNumber and p.isDeleted=false " +
+            "order by p.paidAt desc")
     List<Payment> findAllByOrderNumber(String orderNumber);
 
-    @Query("select p from Payment p join fetch p.consumer c join fetch p.product pd where c.id=:consumerId and p.isDeleted=false order by p.paidAt desc")
+    @Query("select p from Payment p " +
+            "join fetch p.consumer c " +
+            "join fetch c.profile pf " +
+            "join fetch p.product pd " +
+            "join fetch pd.seller s " +
+            "join fetch pd.genre g " +
+            "where c.id=:consumerId and p.isDeleted=false " +
+            "order by p.paidAt desc")
     List<Payment> findAllByConsumerId(Long consumerId);
+
+    @Query("select p from Payment p " +
+            "join fetch p.consumer c " +
+            "join fetch c.profile " +
+            "join fetch p.product pd " +
+            "join fetch pd.seller s " +
+            "join fetch pd.genre g " +
+            "where s.id=:sellerId and p.isDeleted=false " +
+            "order by p.paidAt desc")
+    List<Payment> findAllBySellerId(Long sellerId);
 }
