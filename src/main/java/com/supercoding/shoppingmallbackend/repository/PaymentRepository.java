87a61ccc355e6c4fa2507a5f278080f752c03c47
koeyismyusername <112312898+koeyismyusername@users.noreply.github.com>
2023-08-17 22:43:17 +0900

    Feat: 주문번호로 결제내역 불러오는 쿼리 작성

--- src/main/java/com/supercoding/shoppingmallbackend/repository/PaymentRepository.java
+++ src/main/java/com/supercoding/shoppingmallbackend/repository/PaymentRepository.java
@@ -2,6 +2,7 @@ package com.supercoding.shoppingmallbackend.repository;
 
 import com.supercoding.shoppingmallbackend.entity.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,4 +14,7 @@ public interface PaymentRepository extends JpaRepository<Payment, Long> {
     List<Payment> findByOrderNumberAndIsDeletedIsFalse(String orderNumber);
 
     List<Payment> findAllByConsumerIdAndIsDeletedIsFalse(Long consumerId);
+
+    @Query("select p from Payment p join fetch p.consumer c join fetch p.product pd where p.orderNumber=:orderNumber and p.isDeleted=false order by p.paidAt desc")
+    List<Payment> findAllByOrderNumber(String orderNumber);
 }
