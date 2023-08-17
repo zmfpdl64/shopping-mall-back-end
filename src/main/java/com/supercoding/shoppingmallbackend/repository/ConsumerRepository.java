c5ab3b468c4166986992c8190564e94a0e9669e2
koeyismyusername <112312898+koeyismyusername@users.noreply.github.com>
2023-08-17 22:22:59 +0900

    Feat: consumerId로 profile 찾는 쿼리 작성

--- src/main/java/com/supercoding/shoppingmallbackend/repository/ConsumerRepository.java
+++ src/main/java/com/supercoding/shoppingmallbackend/repository/ConsumerRepository.java
@@ -2,6 +2,7 @@ package com.supercoding.shoppingmallbackend.repository;
 
 import com.supercoding.shoppingmallbackend.entity.Consumer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,4 +11,6 @@ import java.util.Optional;
 public interface ConsumerRepository extends JpaRepository<Consumer, Long> {
 
     Optional<Consumer> findByIdAndIsDeletedIsFalse(Long consumerId);
+    @Query("select p from Consumer c join fetch c.profile p where c.isDeleted=false and p.isDeleted=false and c.id=:consumerId limit 1")
+    Optional<Consumer> findProfileByConsumerId(Long consumerId);
 }
