3409e535b89bc6ed70baa59b4f8a300c88f98949
koeyismyusername <112312898+koeyismyusername@users.noreply.github.com>
2023-08-17 09:52:09 +0900

    Fix: PurchaseHistoryRepository에 잘못 작성되었던 entity 타입 변경

--- src/main/java/com/supercoding/shoppingmallbackend/repository/PurchaseHistoryRepository.java
+++ src/main/java/com/supercoding/shoppingmallbackend/repository/PurchaseHistoryRepository.java
@@ -1,9 +1,10 @@
 package com.supercoding.shoppingmallbackend.repository;
 
+import com.supercoding.shoppingmallbackend.entity.PurchaseHistory;
 import com.supercoding.shoppingmallbackend.entity.ShoppingCart;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PurchaseHistoryRepository extends JpaRepository<ShoppingCart, Long> {
+public interface PurchaseHistoryRepository extends JpaRepository<PurchaseHistory, Long> {
 }
