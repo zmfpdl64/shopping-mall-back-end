a17a05276b0b18e62d7df1b01c9c8c4540b8f0ec
Woojin Lee <69797420+zmfpdl64@users.noreply.github.com>
2023-08-17 17:37:23 +0900

    refactor: Common응답 메소드 추가
    성공했을 때 공통되는 true, 200을 합쳐서
    success()메소드로 묶음

--- src/main/java/com/supercoding/shoppingmallbackend/controller/UserController.java
+++ src/main/java/com/supercoding/shoppingmallbackend/controller/UserController.java
@@ -2,10 +2,15 @@ package com.supercoding.shoppingmallbackend.controller;
 
 import com.supercoding.shoppingmallbackend.common.CommonResponse;
 import com.supercoding.shoppingmallbackend.common.util.ApiUtils;
+import com.supercoding.shoppingmallbackend.dto.request.LoginRequest;
 import com.supercoding.shoppingmallbackend.dto.request.SignupRequest;
+import com.supercoding.shoppingmallbackend.dto.response.LoginResponse;
 import com.supercoding.shoppingmallbackend.service.ProfileService;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -19,18 +24,26 @@ public class UserController {
     private final ProfileService profileService;
 
     @PostMapping("/singup")
-    public CommonResponse<?> signupConsumer(@Valid @RequestBody @ModelAttribute SignupRequest singupRequest,
-                                            @RequestParam("profileImage") MultipartFile profileImage){
+    @io.swagger.v3.oas.annotations.parameters.RequestBody(content = @Content(mediaType = "multipart/form-data",
+            schema = @Schema(implementation = MultipartFile.class)))
+    public CommonResponse<?> signupConsumer(@Valid @ModelAttribute SignupRequest signupRequest,
+                                            @RequestPart(value = "profileImage", required = false) MultipartFile profileImage){
         profileService.signup(
-                singupRequest.getType(),
-                singupRequest.getNickname(),
-                singupRequest.getPassword(),
-                singupRequest.getEmail(),
-                singupRequest.getPhoneNumber(),
+                signupRequest.getType(),
+                signupRequest.getNickname(),
+                signupRequest.getPassword(),
+                signupRequest.getEmail(),
+                signupRequest.getPhoneNumber(),
                 profileImage
         );
 
-        return CommonResponse.builder().result(true).status(200).message("회원가입에 성공했습니다.").build();
+        return CommonResponse.success("회원가입에 성공했습니다.", null);
+    }
+
+    @PostMapping("/login")
+    public CommonResponse<?> login(@RequestBody LoginRequest loginRequest) {
+        LoginResponse loginResponse= profileService.login(loginRequest.getEmail(), loginRequest.getPassword());
+        return CommonResponse.success("로그인에 성공했습니다", loginResponse);
     }
 
 }
