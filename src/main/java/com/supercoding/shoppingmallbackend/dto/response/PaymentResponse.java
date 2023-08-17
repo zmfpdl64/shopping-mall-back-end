63d0c301aa40fbace34a91a09a17bec10b09c693
koeyismyusername <112312898+koeyismyusername@users.noreply.github.com>
2023-08-17 21:51:20 +0900

    Feat: PaymentResponse from 메서드 구현

--- src/main/java/com/supercoding/shoppingmallbackend/dto/response/PaymentResponse.java
+++ src/main/java/com/supercoding/shoppingmallbackend/dto/response/PaymentResponse.java
@@ -2,6 +2,7 @@ package com.supercoding.shoppingmallbackend.dto.response;
 
 import com.supercoding.shoppingmallbackend.common.util.DateUtils;
 import com.supercoding.shoppingmallbackend.dto.request.PaymentRequest;
+import com.supercoding.shoppingmallbackend.entity.Payment;
 import lombok.*;
 
 import java.sql.Timestamp;
@@ -16,11 +17,26 @@ public class PaymentResponse {
     private Long paymentId;
     private String orderNumber;
     private ProductSimpleResponse product;
-    private Long purchaseQuantity;
-    private Long purchasePrice;
+    private Long paidQuantity;
+    private Long paidPrice;
     private Timestamp paidAt;
     private String receivedAddress;
     private String receivedAddressDetail;
     private String recipientName;
     private String recipientPhoneNumber;
+
+    public static PaymentResponse from(Payment payment) {
+        return PaymentResponse.builder()
+                .paymentId(payment.getPaymentId())
+                .orderNumber(payment.getOrderNumber())
+                .product(ProductSimpleResponse.from(payment.getProduct()))
+                .paidQuantity(payment.getPaidQuantity())
+                .paidPrice(payment.getPaidPrice())
+                .paidAt(payment.getPaidAt())
+                .receivedAddress(payment.getReceivedAddress())
+                .receivedAddressDetail(payment.getReceivedAddressDetail())
+                .recipientName(payment.getRecipientName())
+                .recipientPhoneNumber(payment.getRecipientPhoneNumber())
+                .build();
+    }
 }
