7f753077a2744c3bdae59ed3160c9025b65e5fa1
koeyismyusername <112312898+koeyismyusername@users.noreply.github.com>
2023-08-18 09:17:46 +0900

    Refactor: 안 쓰는 코드 제거

--- src/main/java/com/supercoding/shoppingmallbackend/controller/ShoppingCartController.java
+++ src/main/java/com/supercoding/shoppingmallbackend/controller/ShoppingCartController.java
@@ -7,6 +7,7 @@ import com.supercoding.shoppingmallbackend.common.util.ApiUtils;
 import com.supercoding.shoppingmallbackend.dto.request.ShoppingCartItemRequest;
 import com.supercoding.shoppingmallbackend.dto.response.ShoppingCartItemResponse;
 import com.supercoding.shoppingmallbackend.service.ShoppingCartService;
+import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,38 +20,13 @@ public class ShoppingCartController {
 
     private final ShoppingCartService shoppingCartService;
 
-    /*
-     * ====================================================
-     * 아래 필드는 나중에 삭제될 필드
-     * ====================================================
-     */
-    private final boolean isUsingDummyCode = true;
-
     @PostMapping()
     public CommonResponse<ShoppingCartItemResponse> setProduct(@RequestBody ShoppingCartItemRequest shoppingCartItemRequest) {
-
-        if (isUsingDummyCode) {
-            ShoppingCartItemResponse data = ShoppingCartItemResponse.getDummy1();
-
-            return ApiUtils.success("장바구니에 상품을 성공적으로 추가했습니다.", data);
-        }
-
         return shoppingCartService.setProduct(shoppingCartItemRequest);
     };
 
     @GetMapping()
     public CommonResponse<List<ShoppingCartItemResponse>> getShoppingCart(){
-
-        if (isUsingDummyCode) {
-            List<ShoppingCartItemResponse> data = List.of(
-                    ShoppingCartItemResponse.getDummy1(),
-                    ShoppingCartItemResponse.getDummy2(),
-                    ShoppingCartItemResponse.getDummy3()
-            );
-
-            return ApiUtils.success("장바구니를 성공적으로 조회했습니다.", data);
-        }
-
         try {
             return shoppingCartService.getShoppingCart();
         } catch(NumberFormatException e) {
