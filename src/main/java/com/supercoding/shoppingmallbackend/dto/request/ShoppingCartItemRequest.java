a3dfd63327f458ee0df557b1cf5efadfd9dd8715
koeyismyusername <112312898+koeyismyusername@users.noreply.github.com>
2023-08-18 09:43:46 +0900

    Feat: Swagger 작업

--- src/main/java/com/supercoding/shoppingmallbackend/dto/request/ShoppingCartItemRequest.java
+++ src/main/java/com/supercoding/shoppingmallbackend/dto/request/ShoppingCartItemRequest.java
@@ -1,5 +1,7 @@
 package com.supercoding.shoppingmallbackend.dto.request;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
 
 @Getter
@@ -7,7 +9,10 @@ import lombok.*;
 @AllArgsConstructor
 @Builder
 @ToString
+@ApiModel(description = "쇼핑카트에 상품을 저장하기 위해 필요한 정보들을 담은 객체입니다.")
 public class ShoppingCartItemRequest {
+    @ApiModelProperty(required = true, value = "장바구니에 담을 상품의 id", dataType = "Long", example = "1")
     private Long productId;
+    @ApiModelProperty(required = true, value = "장바구니에 담을 상품의 개수", dataType = "Long", example = "1")
     private Long amount;
 }
