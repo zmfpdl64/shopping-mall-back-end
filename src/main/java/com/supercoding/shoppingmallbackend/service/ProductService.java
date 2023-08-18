e7d22ead49c7f86bfcc21551547c4ffa911122ce
no.oneho <oneho.dev@gmail.com>
2023-08-19 02:24:06 +0900

    Refactor: Product 엔티티에 oneToMany 로 하위 엔티티 인덱싱 및 N+1 최적화

--- src/main/java/com/supercoding/shoppingmallbackend/service/ProductService.java
+++ src/main/java/com/supercoding/shoppingmallbackend/service/ProductService.java
@@ -28,20 +28,18 @@ public class ProductService {
     private final GenreRepository genreRepository;
     private final SellerRepository sellerRepository;
     private final CategoryRepository categoryRepository;
-    private final ProductCategoryRepository productCategoryRepository;
     private final ProductContentImageRepository productContentImageRepository;
 
+    @Transactional
     public ProductDetailResponse getProductByProductId(Long productId) {
-        Product product = productRepository.findById(productId).orElseThrow(() -> new CustomException(ProductErrorCode.NOTFOUND_PRODUCT.getErrorCode()));
-        List<ProductContentImage> productContentImageList = productContentImageRepository.findAllByProduct_Id(product.getId());
-        List<ProductImageResponse> productImageResponseList = productContentImageList.stream().map(ProductImageResponse::from).collect(Collectors.toList());
+        Product product = productRepository.findById(productId)
+                .orElseThrow(() -> new CustomException(ProductErrorCode.NOTFOUND_PRODUCT.getErrorCode()));
+        List<ProductContentImage> productContentImageList = product.getProductContentImages();
+        List<ProductImageResponse> productImageResponseList = productContentImageList
+                .stream()
+                .map(ProductImageResponse::from)
+                .collect(Collectors.toList());
         List<Category> categories = categoryRepository.findCategoriesByProductId(product.getId());
-        /*
-        TODO 리뷰 로직
-        List<Long> 타입으로 findRatingByProductID 을 리뷰 엔티티에서 가져옴
-        stream으로 rating 다 더함
-        size만큼 나눠서 소수점 1~2자리
-         */
 
         try {
             return ProductDetailResponse.from(product, productImageResponseList, categories);
@@ -70,21 +68,22 @@ public class ProductService {
         Category difficultyLevel = categoryRepository.findByName(productRequestBase.getDifficultyLevel())
                 .orElseThrow(() -> new CustomException(CategoryErrorCode.NOT_FOUND_BY_ID));
 
-        if (imgFiles.size() > 5) throw new CustomException(ProductErrorCode.TOO_MANY_FILES);
+        boolean imageExists = Optional.ofNullable(imgFiles).isPresent();
+
+        if (imageExists && imgFiles.size() > 5) throw new CustomException(ProductErrorCode.TOO_MANY_FILES);
 
         try {
             Product product = Product.from(productRequestBase, seller, genre);
             productRepository.save(product);
             if (product.getId() != null) {
                 uploadThumbNailImage(thumbFile, product);
+                product.addProductCategory(playerCount);
+                product.addProductCategory(playTime);
+                product.addProductCategory(difficultyLevel);
+                if (imageExists) {
+                    productContentImageRepository.saveAll(uploadImageFileList(imgFiles, product));
+                }
 
-                List<ProductCategory> productCategoryList = new ArrayList<>(Arrays.asList(
-                        ProductCategory.from(product, playerCount),
-                        ProductCategory.from(product, playTime),
-                        ProductCategory.from(product, difficultyLevel)
-                ));
-                productCategoryRepository.saveAll(productCategoryList);
-                productContentImageRepository.saveAll(uploadImageFileList(imgFiles, product));
             }
         } catch (ParseException e) {
             throw new CustomException(CommonErrorCode.INTERNAL_SERVER_ERROR.getErrorCode());
