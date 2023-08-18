4e53668d1f4c53996aeec837725f805efdc23205
koeyismyusername <112312898+koeyismyusername@users.noreply.github.com>
2023-08-18 12:02:45 +0900

    Feat: Swagger 작업

--- src/main/java/com/supercoding/shoppingmallbackend/controller/ShoppingCartController.java
+++ src/main/java/com/supercoding/shoppingmallbackend/controller/ShoppingCartController.java
@@ -7,8 +7,11 @@ import com.supercoding.shoppingmallbackend.common.util.ApiUtils;
 import com.supercoding.shoppingmallbackend.dto.request.ShoppingCartItemRequest;
 import com.supercoding.shoppingmallbackend.dto.response.ShoppingCartItemResponse;
 import com.supercoding.shoppingmallbackend.service.ShoppingCartService;
-import io.swagger.annotations.Api;
+import io.swagger.annotations.*;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,21 +19,53 @@ import java.util.List;
 @RestController
 @RequestMapping("api/v1/shoppingcart")
 @RequiredArgsConstructor
+@Api(
+        tags = {"장바구니", "상품 추가", "상품 수량 변경", "장바구니 조회"},
+        authorizations = @Authorization(value = "Bearer [JWT Token]")
+)
 public class ShoppingCartController {
 
     private final ShoppingCartService shoppingCartService;
 
+    @ApiOperation(
+            value = "장바구니에 상품 세팅(추가 및 수량 변경)",
+            notes = "장바구니에 상품을 추가하거나, 상품의 수량을 변경합니다.",
+            tags = {"상품 추가", "상품 수량 변경"}
+    )
+    @ApiImplicitParams({
+            @ApiImplicitParam(
+                    name = HttpHeaders.AUTHORIZATION,
+                    value = "Bearer [JWT Token]",
+                    required = true,
+                    paramType = "header"
+            )
+    })
     @PostMapping()
-    public CommonResponse<ShoppingCartItemResponse> setProduct(@RequestBody ShoppingCartItemRequest shoppingCartItemRequest) {
+    public CommonResponse<ShoppingCartItemResponse> setProduct(
+            @RequestBody
+            @ApiParam(
+                    required = true,
+                    value = "어떤 상품을 얼마나 담았는지 정보를 담은 객체입니다."
+            )
+            ShoppingCartItemRequest shoppingCartItemRequest) {
         return shoppingCartService.setProduct(shoppingCartItemRequest);
     };
 
+    @ApiOperation(
+            value = "장바구니 전체 조회",
+            notes = "장바구니를 전체 조회합니다.",
+            tags = {"장바구니 조회"}
+    )
+    @ApiImplicitParams({
+            @ApiImplicitParam(
+                    name = HttpHeaders.AUTHORIZATION,
+                    value = "Bearer [JWT Token]",
+                    required = true,
+                    paramType = "header"
+            )
+    })
     @GetMapping()
     public CommonResponse<List<ShoppingCartItemResponse>> getShoppingCart(){
-        try {
-            return shoppingCartService.getShoppingCart();
-        } catch(NumberFormatException e) {
-            throw new CustomException(CommonErrorCode.INVALID_PATH_VARIABLE);
-        }
+        return shoppingCartService.getShoppingCart();
     }
 }
