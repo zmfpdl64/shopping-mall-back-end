52aed1eda1df2256cf83a6f47d17d6ac1b0aa774
koeyismyusername <112312898+koeyismyusername@users.noreply.github.com>
2023-08-18 15:23:33 +0900

    Feat: 장바구니, 결제내역 DTO들 Swagger 작업

--- src/main/java/com/supercoding/shoppingmallbackend/dto/request/ShoppingCartItemRequest.java
+++ src/main/java/com/supercoding/shoppingmallbackend/dto/request/ShoppingCartItemRequest.java
@@ -9,10 +9,10 @@ import lombok.*;
 @AllArgsConstructor
 @Builder
 @ToString
-@ApiModel(value = "쇼핑카트 아이템 객체", description = "어떤 상품을 얼마나 담을지 알려줄 객체")
+@ApiModel("쇼핑카트 아이템 객체")
 public class ShoppingCartItemRequest {
-    @ApiModelProperty(required = true, value = "장바구니에 담을 상품의 id", dataType = "Long", example = "1")
+    @ApiModelProperty(required = true, value = "장바구니에 담을 상품의 id", example = "1")
     private Long productId;
-    @ApiModelProperty(required = true, value = "장바구니에 담을 상품의 개수", dataType = "Long", example = "1")
+    @ApiModelProperty(required = true, value = "장바구니에 담을 상품의 개수", example = "1")
     private Long amount;
 }
--- src/main/java/com/supercoding/shoppingmallbackend/dto/response/ProductSimpleResponse.java
+++ src/main/java/com/supercoding/shoppingmallbackend/dto/response/ProductSimpleResponse.java
@@ -2,6 +2,8 @@ package com.supercoding.shoppingmallbackend.dto.response;
 
 import com.supercoding.shoppingmallbackend.common.util.DateUtils;
 import com.supercoding.shoppingmallbackend.entity.Product;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
 
 @Getter
@@ -9,14 +11,23 @@ import lombok.*;
 @AllArgsConstructor
 @Builder
 @ToString
+@ApiModel("상품 간단 정보")
 public class ProductSimpleResponse {
+    @ApiModelProperty(required = true, value = "상품 id", example = "1")
     private Long id;
+    @ApiModelProperty(required = true, value = "상품 이름", example = "쉐입 퍼즐: 콜로세움 600 PCS")
     private String title;
+    @ApiModelProperty(required = true, value = "상품 썸네일 이미지 URL", example = "https://boardm.co.kr/upload/product/img2/img_largeupfilenm_1688696198_a.jpg")
     private String mainImageUrl;
+    @ApiModelProperty(required = true, value = "상품 장르", example = "퍼즐")
     private String genre;
+    @ApiModelProperty(required = true, value = "상품 가격", example = "20000")
     private Long price;
+    @ApiModelProperty(required = true, value = "상품 재고", example = "9998")
     private Long leftQuantity;
+    @ApiModelProperty(required = true, value = "판매자 id", example = "1")
     private Long sellerId;
+    @ApiModelProperty(required = true, value = "판매 마감 일자", example = "24.08.18")
     private String closingAt;
 
     public static ProductSimpleResponse from(Product product) {
