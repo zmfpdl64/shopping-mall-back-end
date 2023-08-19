9cb123436e6572c8381392f095a23618b2508632
koeyismyusername <112312898+koeyismyusername@users.noreply.github.com>
2023-08-19 11:59:33 +0900

    Feat: 스프링 캐스 적용

--- src/main/java/com/supercoding/shoppingmallbackend/ShoppingMallBackEndApplication.java
+++ src/main/java/com/supercoding/shoppingmallbackend/ShoppingMallBackEndApplication.java
@@ -2,10 +2,12 @@ package com.supercoding.shoppingmallbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy
+@EnableCaching
 public class ShoppingMallBackEndApplication {
 
     public static void main(String[] args) {
