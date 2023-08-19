bcede5524e73684812672b32bbf61b4fa9eae11f
Woojin Lee <69797420+zmfpdl64@users.noreply.github.com>
2023-08-19 14:42:31 +0900

    feat: 페이머니 충전, 조회 기능 구현
    ProfileService: idx로 회원 조회 기능 메소드 추출

---
+++ src/main/java/com/supercoding/shoppingmallbackend/dto/request/RechargeRequest.java
@@ -0,0 +1,12 @@
+package com.supercoding.shoppingmallbackend.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class RechargeRequest {
+    private Long rechargeMoney;
+}
