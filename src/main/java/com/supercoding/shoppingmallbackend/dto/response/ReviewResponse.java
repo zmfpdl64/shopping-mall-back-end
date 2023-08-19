701b1cf292c40b763714ff59df62963a2853a1a1
koeyismyusername <112312898+koeyismyusername@users.noreply.github.com>
2023-08-19 14:33:28 +0900

    Feat: from 메서드 추가

--- src/main/java/com/supercoding/shoppingmallbackend/dto/response/ReviewResponse.java
+++ src/main/java/com/supercoding/shoppingmallbackend/dto/response/ReviewResponse.java
@@ -1,5 +1,6 @@
 package com.supercoding.shoppingmallbackend.dto.response;
 
+import com.supercoding.shoppingmallbackend.entity.Review;
 import lombok.*;
 
 @Getter
@@ -14,4 +15,15 @@ public class ReviewResponse {
     private String reviewImageUrl;
     private String content;
     private Double rating;
+
+    public static ReviewResponse from(Review review) {
+        return ReviewResponse.builder()
+                .id(review.getId())
+                .consumer(ConsumerDetailResponse.from(review.getConsumer()))
+                .product(ProductSimpleResponse.from(review.getProduct()))
+                .reviewImageUrl(review.getReviewImageUrl())
+                .content(review.getContent())
+                .rating(review.getRating())
+                .build();
+    }
 }
