474d98f0f22288ed485527f5dd364cd6368c47a3
koeyismyusername <112312898+koeyismyusername@users.noreply.github.com>
2023-08-19 14:31:47 +0900

    Feat: from 메서드 추가

--- src/main/java/com/supercoding/shoppingmallbackend/dto/response/ProfileResponse.java
+++ src/main/java/com/supercoding/shoppingmallbackend/dto/response/ProfileResponse.java
@@ -1,5 +1,6 @@
 package com.supercoding.shoppingmallbackend.dto.response;
 
+import com.supercoding.shoppingmallbackend.entity.Profile;
 import lombok.*;
 
 @Getter
@@ -10,9 +11,21 @@ import lombok.*;
 public class ProfileResponse {
     private Long id;
     private String email;
-    private String ImageUrl;
+    private String imageUrl;
     private String name;
     private String phoneNumber;
     private Long paymoney;
-    private String Role;
+    private String role;
+
+    public static ProfileResponse from(Profile profile) {
+        return ProfileResponse.builder()
+                .id(profile.getId())
+                .email(profile.getEmail())
+                .imageUrl(profile.getImageUrl())
+                .name(profile.getName())
+                .phoneNumber(profile.getPhone())
+                .paymoney(profile.getPaymoney())
+                .role(profile.getRole().name())
+                .build();
+    }
 }
