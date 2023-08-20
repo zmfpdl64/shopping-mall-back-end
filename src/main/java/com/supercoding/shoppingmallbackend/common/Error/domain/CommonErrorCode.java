d06c20c2b258d5788eaa7cdd86e99323a9a5fe93
koeyismyusername <112312898+koeyismyusername@users.noreply.github.com>
2023-08-20 11:06:33 +0900

    Feat: 에러코드 추가

--- src/main/java/com/supercoding/shoppingmallbackend/common/Error/domain/CommonErrorCode.java
+++ src/main/java/com/supercoding/shoppingmallbackend/common/Error/domain/CommonErrorCode.java
@@ -14,6 +14,7 @@ public enum CommonErrorCode implements ErrorCodeInterface {
     INTERNAL_SERVER_ERROR(500, "Server Error"),
     INVALID_PATH_VARIABLE(HttpStatus.BAD_REQUEST.value(), "형식에 맞지 않는 path variable입니다."),
     INVALID_QUERY_PARAMETER(HttpStatus.BAD_REQUEST.value(), "형식에 맞지 않는 query parameter입니다."),
+    INVALID_QUERY_PARAM_OR_PATH_VARIABLE(HttpStatus.BAD_REQUEST.value(), "형식에 맞지 않는ㄴ path variable 혹은 query parameter입니다."),
     FAIL_TO_SAVE(HttpStatus.INTERNAL_SERVER_ERROR.value(), "서버 측의 문제로 데이터의 저장에 실패했습니다. 다시 한 번 시도해주세요."),
     INVALID_TYPE_VALUE(400, " Invalid Type Value");
 
