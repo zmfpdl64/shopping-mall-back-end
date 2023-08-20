52c34befc435eb4ddb88eff2b9cc996c7283545c
koeyismyusername <112312898+koeyismyusername@users.noreply.github.com>
2023-08-20 10:49:49 +0900

    Feat: consumer로 리뷰 조회 쿼리 추가

--- src/main/java/com/supercoding/shoppingmallbackend/repository/ReviewRepository.java
+++ src/main/java/com/supercoding/shoppingmallbackend/repository/ReviewRepository.java
@@ -1,5 +1,6 @@
 package com.supercoding.shoppingmallbackend.repository;
 
+import com.supercoding.shoppingmallbackend.entity.Consumer;
 import com.supercoding.shoppingmallbackend.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -16,4 +17,12 @@ public interface ReviewRepository extends JpaRepository<Review, Long> {
             "join fetch c.profile pf  " +
             "where p.id=:productId and r.isDeleted=false")
     List<Review> findAllByProductId(long productId);
+
+    @Query("select r from Review r " +
+            "join fetch r.product p " +
+            "join fetch r.consumer c " +
+            "join fetch p.genre g " +
+            "join fetch c.profile pf  " +
+            "where c.id=#{consumer.id} and r.isDeleted=false")
+    List<Review> findAllByConsumer(Consumer consumer);
 }
