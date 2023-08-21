fb72ea84791329ebf3f36c3e18f77464696c980f
koeyismyusername <112312898+koeyismyusername@users.noreply.github.com>
2023-08-21 16:29:05 +0900

    Refactor: 상품리뷰 조회 쿼리 확장

--- src/main/java/com/supercoding/shoppingmallbackend/repository/ReviewRepository.java
+++ src/main/java/com/supercoding/shoppingmallbackend/repository/ReviewRepository.java
@@ -1,12 +1,9 @@
 package com.supercoding.shoppingmallbackend.repository;
 
-import com.supercoding.shoppingmallbackend.dto.response.ReviewResponse;
 import com.supercoding.shoppingmallbackend.entity.Consumer;
+import com.supercoding.shoppingmallbackend.entity.Product;
 import com.supercoding.shoppingmallbackend.entity.Review;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.*;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -16,14 +13,7 @@ import java.util.Optional;
 
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Long> {
-    @Query("select r from Review r " +
-            "join fetch r.product p " +
-            "join fetch r.consumer c " +
-            "join fetch p.genre g " +
-            "join fetch c.profile pf  " +
-            "where p.id=:productId and r.isDeleted=false " +
-            "order by r.createdAt desc")
-    List<Review> findAllByProductId(long productId);
+    List<Review> findAllByProductAndIsDeletedIsFalse(Product product, Sort sort);
 
     @Query(value = "select r from Review r " +
             "join fetch r.product p " +
