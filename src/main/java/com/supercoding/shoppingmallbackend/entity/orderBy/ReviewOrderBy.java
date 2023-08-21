abf70badc017e96a88b111ea7db0d7df7f7a4dce
koeyismyusername <112312898+koeyismyusername@users.noreply.github.com>
2023-08-21 17:18:28 +0900

    Feat: from 메서드 추가

--- src/main/java/com/supercoding/shoppingmallbackend/entity/orderBy/ReviewOrderBy.java
+++ src/main/java/com/supercoding/shoppingmallbackend/entity/orderBy/ReviewOrderBy.java
@@ -1,13 +1,26 @@
 package com.supercoding.shoppingmallbackend.entity.orderBy;
 
+import java.util.Arrays;
+
 public enum ReviewOrderBy {
     CREATION_DATE("creation-date", "createdAt"),
     POPULARITY("popularity", "rating");
     private final String value;
     private final String key;
 
+    public String getKey() { return key; }
+    public String getValue() { return value; }
+
     ReviewOrderBy(String key, String value) {
         this.key = key;
         this.value = value;
+    }
+
+    public static ReviewOrderBy from(String key) {
+        for (ReviewOrderBy e : ReviewOrderBy.values()) {
+            if (e.key.equals(key)) return e;
+        }
+
+        return null;
     }
 }
