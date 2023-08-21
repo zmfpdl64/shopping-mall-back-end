da3925afa640ed093e5b631e795a5bc9bf482bd0
koeyismyusername <112312898+koeyismyusername@users.noreply.github.com>
2023-08-21 09:42:25 +0900

    Feat: 찜하기 기능 추가

--- src/main/java/com/supercoding/shoppingmallbackend/controller/aop/ScrapController.java
+++ src/main/java/com/supercoding/shoppingmallbackend/controller/aop/ScrapController.java
@@ -1,12 +1,40 @@
 package com.supercoding.shoppingmallbackend.controller.aop;
 
+import com.supercoding.shoppingmallbackend.common.CommonResponse;
+import com.supercoding.shoppingmallbackend.common.Error.CustomException;
+import com.supercoding.shoppingmallbackend.common.Error.domain.CommonErrorCode;
+import com.supercoding.shoppingmallbackend.dto.response.ScrapResponse;
+import com.supercoding.shoppingmallbackend.security.AuthHolder;
 import com.supercoding.shoppingmallbackend.service.ScrapService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
+@Api(tags = "찜 목록 API")
 public class ScrapController {
 
     private final ScrapService scrapService;
+
+    @PostMapping("query")
+    @ApiOperation(value = "찜하기", notes = "찜 목록에 추가합니다.")
+    @CacheEvict(value = "scrap", allEntries = true)
+    public CommonResponse<List<ScrapResponse>> addScrap(@RequestParam("productId") Set<String> productIds) {
+        Long profileId = AuthHolder.getUserIdx();
+        try {
+            Set<Long> productIdSet = productIds.stream().map(Long::parseLong).collect(Collectors.toSet());
+            return scrapService.addScrap(profileId, productIdSet);
+        } catch (NumberFormatException e) {
+            throw new CustomException(CommonErrorCode.INVALID_QUERY_PARAMETER);
+        }
+    }
 }
--- src/main/java/com/supercoding/shoppingmallbackend/service/ScrapService.java
+++ src/main/java/com/supercoding/shoppingmallbackend/service/ScrapService.java
@@ -1,12 +1,55 @@
 package com.supercoding.shoppingmallbackend.service;
 
+import com.supercoding.shoppingmallbackend.common.CommonResponse;
+import com.supercoding.shoppingmallbackend.common.Error.CustomException;
+import com.supercoding.shoppingmallbackend.common.Error.domain.ConsumerErrorCode;
+import com.supercoding.shoppingmallbackend.common.Error.domain.ProductErrorCode;
+import com.supercoding.shoppingmallbackend.common.Error.domain.ProfileErrorCode;
+import com.supercoding.shoppingmallbackend.common.util.ApiUtils;
+import com.supercoding.shoppingmallbackend.common.util.JpaUtils;
+import com.supercoding.shoppingmallbackend.dto.response.ConsumerResponse;
+import com.supercoding.shoppingmallbackend.dto.response.ScrapResponse;
+import com.supercoding.shoppingmallbackend.entity.Consumer;
+import com.supercoding.shoppingmallbackend.entity.Product;
+import com.supercoding.shoppingmallbackend.entity.Scrap;
+import com.supercoding.shoppingmallbackend.repository.ConsumerRepository;
+import com.supercoding.shoppingmallbackend.repository.ProductRepository;
 import com.supercoding.shoppingmallbackend.repository.ScrapRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class ScrapService {
 
     private final ScrapRepository scrapRepository;
+    private final ConsumerRepository consumerRepository;
+    private final ProductRepository productRepository;
+
+    public CommonResponse<List<ScrapResponse>> addScrap(Long profileId, Set<Long> productIdSet) {
+        Consumer consumer = getConsumer(profileId);
+
+        List<ScrapResponse> responses = productIdSet.stream()
+                .map(id->productRepository.findProductById(id)
+                        .orElseThrow(()->new CustomException(ProductErrorCode.NOTFOUND_PRODUCT))
+                )
+                .map(product -> {
+                    Scrap newData = Scrap.builder().consumer(consumer).product(product).build();
+                    newData.setIsDeleted(false);
+                    return newData;
+                })
+                .map(scrap -> JpaUtils.managedSave(scrapRepository, scrap))
+                .map(ScrapResponse::from)
+                .collect(Collectors.toList());
+
+        return ApiUtils.success("상품을 성공적으로 찜했습니다.", responses);
+    }
+
+    private Consumer getConsumer(Long profileId) {
+        return consumerRepository.findByProfileId(profileId).orElseThrow(()->new CustomException(ConsumerErrorCode.NOT_FOUND_BY_ID));
+    }
 }
