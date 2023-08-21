0184f9cd43fb312d1dd03987b2142a40d5a24899
koeyismyusername <112312898+koeyismyusername@users.noreply.github.com>
2023-08-21 13:24:44 +0900

    Feat: 찜 목록 페이지네이션 조회 기능 추가

--- src/main/java/com/supercoding/shoppingmallbackend/controller/ScrapController.java
+++ src/main/java/com/supercoding/shoppingmallbackend/controller/ScrapController.java
@@ -3,6 +3,7 @@ package com.supercoding.shoppingmallbackend.controller;
 import com.supercoding.shoppingmallbackend.common.CommonResponse;
 import com.supercoding.shoppingmallbackend.common.Error.CustomException;
 import com.supercoding.shoppingmallbackend.common.Error.domain.CommonErrorCode;
+import com.supercoding.shoppingmallbackend.dto.response.PaginationPageResponse;
 import com.supercoding.shoppingmallbackend.dto.response.ScrapResponse;
 import com.supercoding.shoppingmallbackend.security.AuthHolder;
 import com.supercoding.shoppingmallbackend.service.ScrapService;
@@ -20,7 +21,7 @@ import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-@RestController
+@RestController("/api/v1/scrap-list")
 @RequiredArgsConstructor
 @Api(tags = "찜 목록 API")
 public class ScrapController {
@@ -34,7 +35,18 @@ public class ScrapController {
         return scrapService.getAllScrap(profileId);
     }
 
-    @PostMapping("query")
+    @GetMapping("/query")
+    @ApiOperation(value = "찜한 상품 페이징 조회", notes = "찜 목록 전체를 조회합니다. 그런데 이제 이 페이지네이션을 곁들인...")
+    public CommonResponse<PaginationPageResponse<ScrapResponse>> getScrapPage(@RequestParam("page") String page, @RequestParam("size") String size) {
+        Long profileId = AuthHolder.getUserIdx();
+        try {
+            return scrapService.getScrapPage(profileId, Integer.parseInt(page), Integer.parseInt(size));
+        } catch (NumberFormatException e) {
+            throw new CustomException(CommonErrorCode.INVALID_QUERY_PARAMETER);
+        }
+    }
+
+    @PostMapping("/query")
     @ApiOperation(value = "찜하기", notes = "찜 목록에 추가합니다.")
     public CommonResponse<List<ScrapResponse>> addScrap(@RequestParam("productId") Set<String> productIds) {
         Long profileId = AuthHolder.getUserIdx();
--- src/main/java/com/supercoding/shoppingmallbackend/service/ScrapService.java
+++ src/main/java/com/supercoding/shoppingmallbackend/service/ScrapService.java
@@ -9,6 +9,7 @@ import com.supercoding.shoppingmallbackend.common.Error.domain.ScrapErrorCode;
 import com.supercoding.shoppingmallbackend.common.util.ApiUtils;
 import com.supercoding.shoppingmallbackend.common.util.JpaUtils;
 import com.supercoding.shoppingmallbackend.dto.response.ConsumerResponse;
+import com.supercoding.shoppingmallbackend.dto.response.PaginationPageResponse;
 import com.supercoding.shoppingmallbackend.dto.response.ScrapResponse;
 import com.supercoding.shoppingmallbackend.entity.Consumer;
 import com.supercoding.shoppingmallbackend.entity.Product;
@@ -19,6 +20,9 @@ import com.supercoding.shoppingmallbackend.repository.ScrapRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.Caching;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -34,7 +38,7 @@ public class ScrapService {
     private final ConsumerRepository consumerRepository;
     private final ProductRepository productRepository;
 
-    @Cacheable(value = "scrap", key = "'getAll('+#profileId+')'")
+    @Cacheable(value = "scrap-list", key = "#profileId")
     public CommonResponse<List<ScrapResponse>> getAllScrap(Long profileId) {
         Consumer consumer = getConsumer(profileId);
         List<Scrap> datas = scrapRepository.findAllByConsumerAndIsDeletedIsFalse(consumer);
@@ -42,8 +46,20 @@ public class ScrapService {
         return ApiUtils.success("찜 목록을 성공적으로 조회했습니다.", responses);
     }
 
+    @Cacheable(value = "scrap-page", key = "#profileId+'-'+#page+'-'+#size")
+    public CommonResponse<PaginationPageResponse<ScrapResponse>> getScrapPage(Long profileId, int page, int size) {
+        Consumer consumer = getConsumer(profileId);
+        Page<Scrap> dataPage = scrapRepository.findAllByConsumerAndIsDeletedIsFalse(consumer, PageRequest.of(page, size));
+        List<ScrapResponse> contents = dataPage.getContent().stream().map(ScrapResponse::from).collect(Collectors.toList());
+        PaginationPageResponse<ScrapResponse> response = new PaginationPageResponse<>(dataPage.getTotalPages(), contents);
+        return ApiUtils.success("찜 목록을 성공적으로 조회했습니다.", response);
+    }
+
     @Transactional
-    @CacheEvict(value = "scrap", key = "'getAll('+#profileId+')'")
+    @Caching(evict = {
+        @CacheEvict(value = "scrap-list", key = "#profileId"),
+        @CacheEvict(value = "scrap-page", allEntries = true)
+    })
     public CommonResponse<List<ScrapResponse>> addScrap(Long profileId, Set<Long> productIdSet) {
         Consumer consumer = getConsumer(profileId);
 
@@ -67,6 +83,7 @@ public class ScrapService {
     private Consumer getConsumer(Long profileId) {
         return consumerRepository.findByProfileId(profileId).orElseThrow(()->new CustomException(ConsumerErrorCode.INVALID_PROFILE_ID));
     }
+
 
 
 }
