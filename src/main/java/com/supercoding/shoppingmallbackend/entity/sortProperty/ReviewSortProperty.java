93584da057be161f46af0e8bf9848a104427e385
koeyismyusername <112312898+koeyismyusername@users.noreply.github.com>
2023-08-21 19:17:57 +0900

    Refactor: 클래스명 변경

--- src/main/java/com/supercoding/shoppingmallbackend/entity/sortProperty/ReviewSortProperty.java
+++ src/main/java/com/supercoding/shoppingmallbackend/entity/sortProperty/ReviewSortProperty.java
@@ -1,8 +1,6 @@
-package com.supercoding.shoppingmallbackend.entity.orderBy;
+package com.supercoding.shoppingmallbackend.entity.sortProperty;
 
-import java.util.Arrays;
-
-public enum ReviewOrderBy {
+public enum ReviewSortProperty {
     CREATION_DATE("creation-date", "createdAt"),
     POPULARITY("popularity", "rating");
     private final String value;
@@ -11,20 +9,20 @@ public enum ReviewOrderBy {
     public String getKey() { return key; }
     public String getValue() { return value; }
 
-    ReviewOrderBy(String key, String value) {
+    ReviewSortProperty(String key, String value) {
         this.key = key;
         this.value = value;
     }
 
-    public static ReviewOrderBy from(String key) {
-        for (ReviewOrderBy e : ReviewOrderBy.values()) {
+    public static ReviewSortProperty from(String key) {
+        for (ReviewSortProperty e : ReviewSortProperty.values()) {
             if (e.key.equals(key)) return e;
         }
         return null;
     }
 
     public static String get(String key) {
-        for (ReviewOrderBy e : ReviewOrderBy.values()) {
+        for (ReviewSortProperty e : ReviewSortProperty.values()) {
             if (e.key.equals(key)) return e.getValue();
         }
         return null;
