ae83278a30e1b2ad50c50527b40ff1ee30bd51a3
koeyismyusername <112312898+koeyismyusername@users.noreply.github.com>
2023-08-21 13:24:07 +0900

    Feat: 찜 목록 페이지네이션 조회 쿼리 추가

--- src/main/java/com/supercoding/shoppingmallbackend/repository/ScrapRepository.java
+++ src/main/java/com/supercoding/shoppingmallbackend/repository/ScrapRepository.java
@@ -2,6 +2,8 @@ package com.supercoding.shoppingmallbackend.repository;
 
 import com.supercoding.shoppingmallbackend.entity.Consumer;
 import com.supercoding.shoppingmallbackend.entity.Scrap;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -13,4 +15,5 @@ public interface ScrapRepository extends JpaRepository<Scrap, Long> {
     boolean existsByConsumerAndProductIdAndIsDeletedIsFalse(@NotNull Consumer consumer, @NotNull Long productId);
 
     List<Scrap> findAllByConsumerAndIsDeletedIsFalse(@NotNull Consumer consumer);
+    Page<Scrap> findAllByConsumerAndIsDeletedIsFalse(@NotNull Consumer consumer, Pageable pageable);
 }
