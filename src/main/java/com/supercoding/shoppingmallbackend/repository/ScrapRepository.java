c44037816729dd37f3539f9054142a2087b03d7c
koeyismyusername <112312898+koeyismyusername@users.noreply.github.com>
2023-08-21 12:37:47 +0900

    Feat: 구매자 찜하기 목록 전체 조회 쿼리 추가

--- src/main/java/com/supercoding/shoppingmallbackend/repository/ScrapRepository.java
+++ src/main/java/com/supercoding/shoppingmallbackend/repository/ScrapRepository.java
@@ -5,7 +5,11 @@ import com.supercoding.shoppingmallbackend.entity.Scrap;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ScrapRepository extends JpaRepository<Scrap, Long> {
     boolean existsByConsumerAndProductIdAndIsDeletedIsFalse(Consumer consumer, Long productId);
+
+    List<Scrap> findAllByConsumerIdAndIsDeletedIsFalse(Consumer consumer);
 }
