3335cd4438908240efca304a3e8242fa4b6a5184
koeyismyusername <112312898+koeyismyusername@users.noreply.github.com>
2023-08-22 08:33:28 +0900

    Refactor: 코드 개선

--- src/main/java/com/supercoding/shoppingmallbackend/controller/PaymentsController.java
+++ src/main/java/com/supercoding/shoppingmallbackend/controller/PaymentsController.java
@@ -5,6 +5,7 @@ import com.supercoding.shoppingmallbackend.common.Error.CustomException;
 import com.supercoding.shoppingmallbackend.common.Error.domain.CommonErrorCode;
 import com.supercoding.shoppingmallbackend.dto.request.PaymentRequest;
 import com.supercoding.shoppingmallbackend.dto.response.*;
+import com.supercoding.shoppingmallbackend.security.AuthHolder;
 import com.supercoding.shoppingmallbackend.service.PaymentService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -24,18 +25,22 @@ public class PaymentsController {
     private final PaymentService paymentService;
 
     @PostMapping()
-    public CommonResponse<List<PaymentResponse>> processPayment(@RequestBody @ApiParam(name = "결제 요청 객체", value = "배송 받는 사람의 주소, 이름 ,연락처를 알려줄 객체", required = true) PaymentRequest paymentRequest) {
-        return paymentService.buyWhole(paymentRequest);
+    @ApiOperation(value = "장바구니의 모든 상품 구매", notes = "장바구니의 모든 상품을 구매합니다.")
+    public CommonResponse<List<PaymentResponse>> byWhole(@RequestBody @ApiParam(name = "결제 요청 객체", value = "배송 받는 사람의 주소, 이름 ,연락처를 알려줄 객체", required = true) PaymentRequest paymentRequest) {
+        Long profileId = AuthHolder.getProfileIdx();
+        return paymentService.buyWhole(profileId, paymentRequest);
     }
 
     @ApiOperation(value = "일부 상품 결제하기", notes = "현재 장바구니에 담긴 상품 중 지정된 것을 구매합니다.")
     @PostMapping("/query")
     public CommonResponse<List<PaymentResponse>> processPaymentSelected(
             @RequestParam("id") Set<String> shoppingCartIds,
-            @RequestBody @ApiParam(name = "결제 요청 객체", value = "배송 받는 사람의 주소, 이름 ,연락처를 알려줄 객체", required = true) PaymentRequest paymentRequest) {
+            @RequestBody @ApiParam(name = "결제 요청 객체", value = "배송 받는 사람의 주소, 이름 ,연락처를 알려줄 객체", required = true) PaymentRequest paymentRequest
+    ){
+        Long profileId = AuthHolder.getProfileIdx();
         try {
             Set<Long> shoppingCartIdSet = shoppingCartIds.stream().map(Long::parseLong).collect(Collectors.toSet());
-            return paymentService.buySelected(paymentRequest, shoppingCartIdSet);
+            return paymentService.buySelected(profileId, paymentRequest, shoppingCartIdSet);
         } catch (NumberFormatException e) {
             throw new CustomException(CommonErrorCode.INVALID_QUERY_PARAMETER);
         }
@@ -44,14 +49,16 @@ public class PaymentsController {
     @ApiOperation(value = "구매내역 가져오기", notes = "구매자의 구매내역을 가져옵니다.")
     @GetMapping("/purchased")
     public CommonResponse<List<PurchaseResponse>> getPurchaseHistory() {
-        return paymentService.getPurchaseHistory();
+        Long profileId = AuthHolder.getProfileIdx();
+        return paymentService.getPurchaseHistory(profileId);
     }
 
     @ApiOperation(value = "구매내역 가져오기 (pagination)", notes = "구매자의 구매내역을 가져옵니다. 그런데 이제 이 pagination을 곁들인...")
     @GetMapping("/purchased/query")
     public CommonResponse<PaginationResponse<PurchaseResponse>> getPurchaseHistory(@RequestParam String page, @RequestParam String size) {
+        Long profileId = AuthHolder.getProfileIdx();
         try {
-            return paymentService.getPurchaseHistoryWithPagination(Integer.parseInt(page), Integer.parseInt(size));
+            return paymentService.getPurchaseHistoryWithPagination(profileId, Integer.parseInt(page), Integer.parseInt(size));
         } catch(NumberFormatException e) {
             throw new CustomException(CommonErrorCode.INVALID_QUERY_PARAMETER);
         }
@@ -60,14 +67,16 @@ public class PaymentsController {
     @ApiOperation(value = "판매내역 가져오기", notes = "판매자의 판매내역을 가져옵니다.")
     @GetMapping("/sold")
     public CommonResponse<List<SaleResponse>> getSaleHistory() {
-        return paymentService.getSaleHistory();
+        Long profileId = AuthHolder.getProfileIdx();
+        return paymentService.getSaleHistory(profileId);
     }
 
     @ApiOperation(value = "판매내역 가져오기 (pagination)", notes = "판매자의 판매내역을 가져옵니다. 그런데 이제 이 pagination을 곁들인...")
     @GetMapping("/sold/query")
     public CommonResponse<PaginationResponse<SaleResponse>> getSaleHistory(@RequestParam String page, @RequestParam String size) {
+        Long profileId = AuthHolder.getProfileIdx();
         try {
-            return paymentService.getSaleHistoryWithPagination(Integer.parseInt(page), Integer.parseInt((size)));
+            return paymentService.getSaleHistoryWithPagination(profileId, Integer.parseInt(page), Integer.parseInt((size)));
         } catch (NumberFormatException e) {
             throw new CustomException(CommonErrorCode.INVALID_QUERY_PARAMETER);
         }
