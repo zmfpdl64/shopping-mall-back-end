a83591e43f671a958be7561795f1a5ddc02b3e28
no.oneho <oneho.dev@gmail.com>
2023-08-22 22:27:14 +0900

    Chore: 노출 되던 실행 쿼리 비노출로 변경, TimeTrace 어노테이션 개발하여 메서드 실행 시간 확인

--- src/main/java/com/supercoding/shoppingmallbackend/repository/AddressRepository.java
+++ src/main/java/com/supercoding/shoppingmallbackend/repository/AddressRepository.java
@@ -1,5 +1,6 @@
 package com.supercoding.shoppingmallbackend.repository;
 
+import com.supercoding.shoppingmallbackend.common.TimeTrace;
 import com.supercoding.shoppingmallbackend.entity.Address;
 import com.supercoding.shoppingmallbackend.entity.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,6 +12,7 @@ import java.util.Optional;
 @Repository
 public interface AddressRepository extends JpaRepository<Address, Long> {
 
+    @TimeTrace
     Optional<Address> findByProfile(Profile profile);
 
 }
