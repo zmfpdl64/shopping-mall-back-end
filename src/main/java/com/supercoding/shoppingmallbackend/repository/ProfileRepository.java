f273837f2aa54db04e7c1bb18247638122240bd7
Woojin Lee <69797420+zmfpdl64@users.noreply.github.com>
2023-08-23 15:14:23 +0900

    feat: 휴대폰 유니크값 변경 임시 메소드 작성

--- src/main/java/com/supercoding/shoppingmallbackend/repository/ProfileRepository.java
+++ src/main/java/com/supercoding/shoppingmallbackend/repository/ProfileRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -23,4 +24,7 @@ public interface ProfileRepository extends JpaRepository<Profile, Long> {
 
     @Query("SELECT p FROM Profile p WHERE p.phone = :phoneNum")
     Optional<Profile> findByPhoneNum(@Param("phoneNum") String phoneNum);
+
+    @Query("SELECT p FROM Profile p")
+    List<Profile> findAllProfile();
 }
