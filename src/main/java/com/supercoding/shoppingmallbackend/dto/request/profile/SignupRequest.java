763f36b812ef9231ad15b0722c88aaef0accef39
Woojin Lee <69797420+zmfpdl64@users.noreply.github.com>
2023-08-23 15:14:36 +0900

    chore: Profile 파트 스웨거 문서 작성

--- src/main/java/com/supercoding/shoppingmallbackend/dto/request/profile/SignupRequest.java
+++ src/main/java/com/supercoding/shoppingmallbackend/dto/request/profile/SignupRequest.java
@@ -4,6 +4,8 @@ import com.supercoding.shoppingmallbackend.common.Error.CustomException;
 import com.supercoding.shoppingmallbackend.common.Error.domain.ProfileErrorCode;
 import com.supercoding.shoppingmallbackend.common.Error.domain.UserErrorCode;
 import com.supercoding.shoppingmallbackend.common.util.Regex;
+import io.swagger.annotations.ApiModelProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.web.multipart.MultipartFile;
@@ -15,18 +17,24 @@ import javax.validation.constraints.Pattern;
 @AllArgsConstructor
 @Getter
 public class SignupRequest {
+
+    @ApiModelProperty(value = "회원 타입 SELLER | CONSUMER 중에 선택해주세요", required = true, example = "CONSUMER")
     @NotBlank(message = "SELLER | CONSUMER 중에 선택해주세요")
     private String type;
 
+    @ApiModelProperty(value = "이메일 example@email.com", required = true, example = "example@email.com")
     @Pattern(regexp = "^[A-Za-z0-9+_-]+@+[A-Za-z0-9+_.-]+", message = "이메일 형식이 아닙니다.")
     private String email;
 
+    @ApiModelProperty(value = "비밀번호 test1234", required = true, example = "test1234")
     @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z]).{8,20}", message="숫자, 영어 8자 이상 작성해주세요")
     private String password;
 
+    @ApiModelProperty(value = "이름 홍길동", required = true, example = "홍길동")
     @NotBlank(message="이름은 필수 항목입니다.")
     private String nickname;
 
-    @Pattern(regexp = "[0-9]{2,3}-[0-9]{3,4}-[0-9]{3,4}", message = "12~13자리의 -와 숫자를 입력가능합니다")
+    @ApiModelProperty(value = "휴대폰 번호 010-1234-1234", required = true, example = "010-1234-1234")
+    @Pattern(regexp = "[0-9]{2,3}-[0-9]{3,4}-[0-9]{3,4}", message = "10~13자리의 -와 숫자를 입력가능합니다")
     private String phoneNumber;
 }
--- src/main/java/com/supercoding/shoppingmallbackend/dto/request/profile/UpdatePasswordRequest.java
+++ src/main/java/com/supercoding/shoppingmallbackend/dto/request/profile/UpdatePasswordRequest.java
@@ -1,5 +1,6 @@
 package com.supercoding.shoppingmallbackend.dto.request.profile;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,7 +9,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Getter
 public class UpdatePasswordRequest {
+
+    @ApiModelProperty(value = "이메일", required = true, example = "asdf1234z@email.com1")
     private String email;
+    @ApiModelProperty(value = "비밀번호", required = true, example = "asdf1234z")
     private String password;
+    @ApiModelProperty(value = "비밀번호", required = true, example = "asdf1234z")
     private String updatePassword;
 }
