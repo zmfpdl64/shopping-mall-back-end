5410efe9282f5314e851c75f8c00522063f98e60
doit-zero <131241458+doit-zero@users.noreply.github.com>
2023-08-24 02:00:10 +0900

    Refactor : 답변 수정,삭제 리펙토링

--- src/main/java/com/supercoding/shoppingmallbackend/controller/AnswerController.java
+++ src/main/java/com/supercoding/shoppingmallbackend/controller/AnswerController.java
@@ -4,8 +4,7 @@ import com.supercoding.shoppingmallbackend.common.CommonResponse;
 import com.supercoding.shoppingmallbackend.common.util.ApiUtils;
 import com.supercoding.shoppingmallbackend.dto.request.answer.CreateAnswerRequest;
 import com.supercoding.shoppingmallbackend.dto.request.answer.UpdateAnswerRequest;
-import com.supercoding.shoppingmallbackend.dto.response.answer.CreateAnswerResponse;
-import com.supercoding.shoppingmallbackend.dto.response.answer.UpdateAnswerResponse;
+import com.supercoding.shoppingmallbackend.security.AuthHolder;
 import com.supercoding.shoppingmallbackend.service.AnswerService;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
@@ -15,30 +14,36 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/questions/answers")
 public class AnswerController {
+
     private final AnswerService answerService;
 
-    @ApiOperation(value = "문의 답변을 작성.")
+    @ApiOperation(value = "문의 답변 작성")
     @PostMapping
     public CommonResponse<Object> createAnswer(
-            @RequestBody CreateAnswerRequest request){
-        CreateAnswerResponse createAnswerResponse = answerService.createAnswer(request);
-        return ApiUtils.success("답변 작성 완료",createAnswerResponse);
+            @ModelAttribute CreateAnswerRequest createAnswerRequest){
+        Long userIdx = AuthHolder.getProfileIdx();
+        answerService.createAnswer(createAnswerRequest,userIdx);
+        return ApiUtils.success("작성 완료",null);
     }
-    @ApiOperation(value = "문의 답변을 수정.")
-    @PutMapping("/{id}")
+
+    @ApiOperation(value = "답변 수정")
+    @PostMapping("/{answer_idx}")
     public CommonResponse<Object> updateAnswer(
-            @PathVariable Long id,
-            @RequestBody UpdateAnswerRequest request) {
-        UpdateAnswerResponse updateAnswer = answerService.updateAnswer(id,request);
-        if( updateAnswer == null ){
-            return ApiUtils.fail(404,"존재하지 않습니다.");
-        }
-        return ApiUtils.success("수정완료",updateAnswer);
+            @PathVariable("answer_idx") Long answerId,
+            @ModelAttribute UpdateAnswerRequest updateAnswerRequest){
+        Long profileIdx = AuthHolder.getProfileIdx();
+        answerService.updateAnswerByAnswerId(answerId,profileIdx,updateAnswerRequest);
+        return ApiUtils.success(answerId+"번 답변 수정 성공",null);
+
     }
-    @ApiOperation(value = "문의 답변을 삭제.")
-    @DeleteMapping("/{id}")
-    public CommonResponse<Object> deleteAnswer(@PathVariable Long id){
-        answerService.deleteAnswer(id);
-        return ApiUtils.success("삭제 완료",null);
+
+    @ApiOperation(value = "답변 삭제")
+    @DeleteMapping("/{answer_idx}")
+    public CommonResponse<Object> deleteQuestion(
+            @PathVariable("answer_idx") Long answerId) {
+        Long profileIdx = AuthHolder.getProfileIdx();
+        answerService.deleteAnswerByAnswerId(answerId,profileIdx);
+        return ApiUtils.success(answerId + "번 문의 삭제 성공", null);
     }
+
 }
